2bf5d1f6b1a2abb50646a39ea0de18ee59d36b8e
thiagotfsilva <thiagophsilva@hotmail.com>
2025-06-03 22:39:13 -0300

    implementando o metodo insert para um lista duplamente ligada

--- 4 - listas_ligadas/doublyLinkedList.js
+++ 4 - listas_ligadas/doublyLinkedList.js
@@ -1,5 +1,6 @@
 import { defaultEquals } from "../utils/utils.js";
 import LinkedList from "./linkedList.js";
+import DoublyNode from './doublyNode.js';
 
 
 class DoublyLinkedList extends LinkedList {
@@ -10,7 +11,38 @@ class DoublyLinkedList extends LinkedList {
   }
 
   insert(element, index) {
+    if(index >= 0 && index <= this.count) {
+      const node = new DoublyNode(element);
+      let current = this.head;
+      if(index === 0) { // inserir um elemneto no início da fila.
+        if(this.head == null) {
+          this.head = node;
+          this.tail = node;
+        } else {
+          node.next = this.head;
+          current.prev = node;
+          this.head = node; // primeiro elemento da lista
+        }
+      } else if(index === this.count){ // inserir no final da fila
+        current = this.tail;
+        current.next = node;
+        node.prev = current;
+        this.tail = node; // ultimo elemento
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        node.next = current;
+        previous.next = node;
+        current.prev = node;
+        node.prev = previous;
+      }
 
+      this.count++;
+
+      return true;
+    }
+
+    return false;
   }
 }
 
