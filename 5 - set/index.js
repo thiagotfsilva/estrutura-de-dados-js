bbe4662d24055c7574a5082f485e0cede0735860
thiagotfsilva <thiagophsilva@hotmail.com>
2025-07-29 19:44:16 -0300

    finalizando classe set, criando metedos de uniao, interseccao e diferenca

--- 5 - set/index.js
+++ 5 - set/index.js
@@ -19,3 +19,34 @@ set.delete(2);
 console.log(set.values());
 console.log(set.size()); 
 
+
+console.log("**************************************");
+
+const setA = new Set();
+setA.add(1);
+setA.add(3);
+setA.add(5);
+setA.add(7);
+setA.add(9);
+
+const setB = new Set();
+setB.add(2);
+setB.add(4);
+setB.add(6);
+setB.add(8);
+
+const setC = new Set();
+setC.add(2);
+setC.add(3);
+setC.add(5);
+setC.add(7);
+
+const setUnion = setA.union(setB);
+console.log(setUnion.values());
+
+const setIntercetion = setA.intersection(setC);
+console.log(setIntercetion.values());
+
+const setDifference = setB.difference(setC);
+console.log(setDifference.values());
+
--- 5 - set/set.js
+++ 5 - set/set.js
@@ -63,9 +63,67 @@ class Set {
     return values;
   };
 
+  // função pura. 
+  // não modificam a instância atual da classe set. são funcoes puras, 
+  // apenas geram um novo resultado.
   union(otherSet) {
-
+    const unionSet = new Set();
+    this.values().forEach(value => unionSet.add(value));
+    otherSet.values().forEach(value => unionSet.add(value));
+    return unionSet;
   };
+
+  intersection(otherSet) {
+    const intersectionSet = new Set();
+
+    const values = this.values();
+    const otherValues = otherSet.values();
+
+    let biggerSet = values;
+    let smallerSet = otherValues;
+
+    if (otherValues.length - values.length > 0) {
+      biggerSet = otherValues;
+      smallerSet = values;
+    }
+
+    smallerSet.forEach(value => {
+      if (biggerSet.includes(value)) {
+        intersectionSet.add(value);
+      }
+    });
+
+    return intersectionSet;
+  };
+
+  difference(otherSet) {
+    const differenceSet = new Set();
+    this.values().forEach(value => {
+      if (!otherSet.has(value)) {
+        differenceSet.add(value);
+      }
+    });
+
+    return differenceSet;
+  }
+
+  isSubsetOf(otherSet) {
+    if (this.size() > otherSet.size()) {
+      return false;
+    }
+
+    let isSubSet = true;
+    this.values().every(value => {
+      if (!otherSet.has(value)) {
+        isSubSet = false;
+        return false;
+      }
+
+      return true;
+    });
+
+    return isSubSet;
+  }
 
 }
 
